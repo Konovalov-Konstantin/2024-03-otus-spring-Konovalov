6154c92729958afa39a8483cf3a6a41db6000656
Konstantin <kon-kos87@yandex.ru>
2024-07-15 08:20:46 +0500

    homework_12 - правки

--- hw12-Spring_Security/src/main/java/ru/otus/hw/Application.java
+++ hw12-Spring_Security/src/main/java/ru/otus/hw/Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    /**
+     Аутентификация:
+     admin / admin
+     user / user
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
